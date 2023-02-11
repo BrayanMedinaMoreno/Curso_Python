ee9dae1b452abc49cb75c85d3189952ba7117eb1
BrayanMedinaMoreno <brayan0519@yahoo.com>
2023-02-10 20:34:12 -0500

    Primer Dia

---
+++ 00_Inicio.py
@@ -0,0 +1,23 @@
+# Esto es un comentario:
+# Hola mundo en python
+print("Hola Python")
+
+"""
+Comentario
+en multiples
+lineas
+
+"""
+
+# Consultar el tipo de dato
+
+print (type("Soy un dato str")) # Tipo Dato 'Str'
+print (type(4)) # Tipo Dato 'Int'
+print (type(1.4)) # Tipo Dato 'Float'
+print (type(True)) # Tipo Dato 'Bool'
+print (type(1 + 1j)) # Tipo Dato 'Complejo'
+print (type([1,2,3])) # Tipo Dato 'List'
+print (type({'name' : 'asabenneh'})) #Dictionary
+
+
+
---
+++ 01_variables.py
@@ -0,0 +1,39 @@
+# Variables
+
+my_string_variable = "My String Variable"
+print(my_string_variable)
+
+my_int_variable = 5
+print(my_int_variable)
+
+my_bool_variable = False
+print (my_bool_variable)
+
+# Cambiar Tipo de variable
+
+my_int_to_variable = str(my_int_variable)
+print(my_int_to_variable)
+print(type(my_int_to_variable))
+
+# Imprimir variables tipo string
+
+print (my_string_variable, my_int_variable, my_bool_variable)
+
+# Variables en una sola linea
+
+nombre, apellido, alias, edad = "brayan", "medina", "akari", 23
+
+# Concatenacion de variables en un print
+
+print("me llamo",nombre ,"mi apellido es",apellido ,"mi apodo es" ,alias, "tengo de edad", edad)
+print(type(edad))
+
+# Algunas funciones del sistema len()
+print (len(my_string_variable))
+
+minion = ""
+
+
+
+
+
