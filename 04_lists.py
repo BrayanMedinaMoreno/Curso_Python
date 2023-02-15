bb68dc0889cdac869f41f4681a8592260cfce495
BrayanMedinaMoreno <brayan0519@yahoo.com>
2023-02-14 19:50:49 -0500

    listas

---
+++ 04_lists.py
@@ -0,0 +1,22 @@
+### Lists ###
+
+my_list = list()
+my_other_list = []
+
+print(len(my_list))
+
+my_list = [23, 12, 32, 52, 51]
+
+print(my_list)
+print(len(my_list))
+
+my_other_list = [23, 1.80, "brayan", "medina"]
+
+print(type(my_other_list))
+print(type(my_list))
+
+print(my_other_list [0])
+print(my_other_list [1])
+print(my_other_list [-1])
+print(my_other_list [-4])
+print(my_other_list [0])
