b6860e3ff606e0f1ec490653ed92a25ca92c0372
BrayanMedinaMoreno <brayan0519@yahoo.com>
2023-02-21 21:19:37 -0500

    XD

---
+++ Python flask + MongoDB/src/app.py
@@ -0,0 +1,43 @@
+from flask import Flask, request
+from flask_pymongo import PyMongo
+from werkzeug.security import generate_password_hash,check_password_hash
+
+app = Flask(__name__)   
+app.config["MONGO_URI"]="mongodb://localhost:27017/local"
+mongo = PyMongo(app)
+
+#Home#
+
+@app.route("/", methods=["GET"])
+def create_Home():
+    return "received" 
+
+
+#post#
+@app.route("/users", methods=["POST"])
+def create_User():
+    # recive datos
+    username = request.json["username"]
+    password = request.json["password"]
+    email = request.json["email"]
+    if username and email and password:
+        hashed_password = generate_password_hash(password)
+        id = mongo.db.users.insertOne(
+            {"username": username,"email": email,"password": hashed_password}
+        )
+        response = {
+            "id": str(id),
+            "username": username,
+            "password": hashed_password,
+            "email": email
+        }
+        return response
+    else:
+        {"message":"received"}
+        
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
+    
