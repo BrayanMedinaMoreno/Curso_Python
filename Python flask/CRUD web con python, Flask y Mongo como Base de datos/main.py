5026ec224c5ddf4f28462d2582d1a9c280727307
BrayanMedinaMoreno <brayan0519@yahoo.com>
2023-03-10 14:29:49 -0500

    XD

--- Python flask/CRUD web con python, Flask y Mongo como Base de datos/main.py
+++ Python flask/CRUD web con python, Flask y Mongo como Base de datos/main.py
@@ -7,3 +7,4 @@ if __name__=="__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
     
 #Aqui podemos definir la ip y el puerto que usaremos, vemos como se hace, el host 0.0.0.0 es local=localhost o 172.0.0.1
+# https://www.youtube.com/watch?v=affNd5VpJBQ
